ba6e767d6d8f151f467df5c91c2b93c6f22b36a5
Andy Collins <askingalot@gmail.com>
2021-01-15 08:14:14 -0600

    modularized it

---
+++ FavoriteMovieDisplay.js
@@ -0,0 +1,27 @@
+export const renderFavoriteMovieDisplay = (favoriteMovie) => {
+  const htmlElement = document.querySelector(".favorite-movie-display-container");
+  htmlElement.innerHTML = `
+    <h1>${favoriteMovie}</h1>
+    <button id="clear-favorite-movie-btn">Clear</button>
+  `;
+};
+
+export const clearDisplay = () => {
+  const displayElement = document.querySelector(".favorite-movie-display-container");
+  displayElement.innerHTML = '';
+}
+
+export const addClearEventListener = () => {
+  const container = document.querySelector('.favorite-movie-display-container');
+  container.addEventListener('click', (evt) => {
+    if (evt.target.id !== 'clear-favorite-movie-btn') {
+      return
+    }
+
+    sessionStorage.removeItem('favoriteMovie')
+
+    const eventHub = document.querySelector('.container');
+    eventHub.dispatchEvent(new CustomEvent('favoriteMovieChanged'));
+  });
+}
+
---
+++ favoriteMovieForm.js
@@ -0,0 +1,31 @@
+export const clearForm = () => {
+  const formElement = document.querySelector(".favorite-movie-form-container");
+  formElement.innerHTML = '';
+}
+
+export const addFavoriteMovieEventListener = () => {
+  const container = document.querySelector('.favorite-movie-form-container');
+  container.addEventListener('click', (evt) => {
+    if (evt.target.id !== 'favorite-movie-save-btn') {
+      return;
+    }
+
+    const favoriteMovieInput = document.querySelector('#favorite-movie');
+    sessionStorage.setItem('favoriteMovie', favoriteMovieInput.value);
+
+    const eventHub = document.querySelector('.container');
+    eventHub.dispatchEvent(new CustomEvent('favoriteMovieChanged'));
+  });
+}
+
+export const renderFavoriteMovieForm = () => {
+  const htmlElement = document.querySelector(".favorite-movie-form-container");
+  htmlElement.innerHTML = `
+      <fieldset>
+        <p>
+          <label for"favorite-movie">Favorite Movie:</label>
+          <input id="favorite-movie" placeholder="Enter your favorite movie"/>
+        </p>
+        <input id="favorite-movie-save-btn" type="submit" value="Record Favorite Movie" />
+      </fieldset>`;
+}
